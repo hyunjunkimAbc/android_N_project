38d0368ff63cbf8e309134d05f0edf71272d7cba
H-Beom <sju05254@hansung.ac.kr>
2022-11-27 19:57:49 +0900

    First commit to connect with GithHub

---
+++ app/src/main/java/com/example/androidteamproject/ShowFriendPosting.kt
@@ -0,0 +1,11 @@
+package com.example.androidteamproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowFriendPosting : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
